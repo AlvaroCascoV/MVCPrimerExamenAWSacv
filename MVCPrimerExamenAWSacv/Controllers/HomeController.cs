8d3f6b4e1c457076d4b8a8dc55ea917e03313273
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-06-03 09:41:43 +0200

    Add project files.

---
+++ MVCPrimerExamenAWSacv/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCPrimerExamenAWSacv.Models;
+using System.Diagnostics;
+
+namespace MVCPrimerExamenAWSacv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
